140b32002fa4f70e301f98e9eef7a08ec991a649
Apprentice <apprentice@devbootcamp.com>
2013-08-27 20:19:57 -0700

    added category

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,23 @@
 get '/' do
-  # Look in app/views/index.erb
+  
   erb :index
 end
+
+get '/category/' do
+  p params
+  @category_names = Category.all
+  p @category_names
+  erb :category
+end
+
+get '/create/' do
+  erb :create
+end
+
+post '/create/' do
+  p params
+  Post.create(title: params[:title], content: params[:content])
+  erb :create
+end
+
+
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# Category.new(name: 'Cars')
+# Category.new(name: 'Rentals')
+# Category.new(name: 'Services')
+# Category.new(name: 'Jobs')
+# Category.new(name: 'Tutoring')
+# Category.new(name: 'Programming')
+
+
